6965ab4c3d4c80182bdfc7ec51e12b5869086ebf
Mike Sullihan <sullyut@gmail.com>
2023-09-09 17:46:20 -0500

    Winnie Base

--- sullvantes/src/App.js
+++ sullvantes/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
 import logo from './logo.svg';
+import winstar from './winstar.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Sullvantes Portfolio</h1> 
+        <img src={winstar} className="App-logo" alt="logo" />
         <p>
           
         </p>
